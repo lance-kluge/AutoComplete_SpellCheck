0df1ae9df957dbe1c413bf2386081400eeb4d9ae
Lance Kluge <lancekluge@Lances-MacBook-Air.local>
2024-11-02 12:44:52 -0600

    adds working spell check and autocomplete

--- AutoCompleteDict.py
+++ AutoCompleteDict.py
@@ -1,6 +1,8 @@
 from TrieNode import TrieNode
 
 
+
+
 class Trie:
     def __init__(self):
         self.root = TrieNode()
@@ -39,6 +41,42 @@ class Trie:
         return results
 
 
+    def oneAwayWords(self, word):
+        node = self.root
+        lim = 0
+        results = []
+        self.insertion(word, results)
+        self.deletion(word, results)
+        self.substitution(word, results)
+        return results
+
+    def insertion(self, word, retArray):
+        for i in range(len(word)+1):
+            for char in self.root.getNextCharacters():
+                if self.isWord(''.join(word[:i] + char + word[i:])):
+                    retArray.append(''.join(word[:i] + char + word[i:]))
+
+    def isWord(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.getNextCharacters():
+                return False
+            node = node.getChild(char)
+        if node.isWord() == True:
+            return True
+        return False
+    def deletion(self, word, retArray):
+        for i in range(len(word)):
+            if self.isWord(''.join(word[:i] + word[i+1:])) and ''.join(word[:i] + word[i+1:]) not in retArray:
+                retArray.append(''.join(word[:i] + word[i+1:]))
+    def substitution(self, word, retArray):
+        for i in range(len(word)):
+            for char in self.root.getNextCharacters():
+                if self.isWord(''.join(word[:i] + char + word[i+1:])):
+                    retArray.append(''.join(word[:i] + char + word[i+1:]))
+
+
+
 def read_file_to_trie(file_path):
     trie = Trie()
     with open(file_path, 'r') as file:
@@ -53,5 +91,19 @@ if __name__ == "__main__":
     print("Words have been added to the trie.")
     test_word = ''
     while test_word != 'exit':
-        test_word = input("Enter a word to autocomplete: (exit to stop)")
-        print(trie.autocomplete(test_word))
+        print('For spelling suggestions, type 1, for autocomplete press 2:')
+        input_Int = input()
+        if input_Int == '1':
+            test_word = input("Enter a word for spell suggestions: (exit to stop)")
+            if trie.isWord(test_word):
+                print("That is already a word")
+            else:
+                print(trie.oneAwayWords(test_word))
+        elif input_Int == '2':
+            test_word = input("Enter a word for autocomplete suggestions:")
+            if trie.isWord(test_word):
+                print("That is already a word")
+            else:
+                print(trie.autocomplete(test_word))
+        else:
+            print("Invalid input")
